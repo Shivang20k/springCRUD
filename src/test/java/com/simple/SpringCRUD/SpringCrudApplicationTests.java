63c6fbbc81927d13930fa2fca8b9a5721ef04680
Shivang <st.shivanggupta2@gmail.com>
2023-10-01 14:04:36 +0530

    Project PUSH

---
+++ src/test/java/com/simple/SpringCRUD/SpringCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simple.SpringCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
