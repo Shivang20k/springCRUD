0c533e9db82b687b741f2de6194f09b2a59884a3
Shivang <st.shivanggupta2@gmail.com>
2023-10-01 16:55:17 +0530

    Making Controller Methods

---
+++ src/main/java/com/simple/SpringCRUD/controller/APIController.java
@@ -0,0 +1,67 @@
+package com.simple.SpringCRUD.controller;
+
+import com.simple.SpringCRUD.exception.ResourceNotFoundException;
+import com.simple.SpringCRUD.model.Passenger;
+import com.simple.SpringCRUD.repository.PassengerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class APIController {
+
+    @Autowired
+    private PassengerRepository passengerRepository;
+
+    @GetMapping("/passenger")
+    private ResponseEntity<List<Passenger>> getAllPassengers() {
+        return new ResponseEntity<>(passengerRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/passenger/{id}")
+    private ResponseEntity<Passenger> getPassengerById(@PathVariable long id) {
+        Passenger passenger;
+        try {
+            passenger = passengerRepository.findById(id)
+                    .orElseThrow(() -> new ResourceNotFoundException("Passenger does not exist by ID" + id));
+        } catch (ResourceNotFoundException e) {
+            return new ResponseEntity<>(new Passenger(), HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(passenger, HttpStatus.FOUND);
+    }
+
+    @PostMapping("/passenger")
+    private ResponseEntity<String> createPassenger(@RequestBody Passenger passenger) {
+         passengerRepository.save(passenger);
+         return new ResponseEntity<>("Creation successfully", HttpStatus.OK);
+    }
+
+    @PutMapping("/passenger/update/{id}")
+    private ResponseEntity<Passenger> updatePassenger(@PathVariable long id, @RequestBody Passenger passenger) {
+        Passenger ps =  passengerRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Passenger does not exist by ID" + id));
+        ps.setId(passenger.getId());
+        ps.setMail(passenger.getMail());
+        ps.setName(passenger.getName());
+        ps.setPhone(passenger.getPhone());
+        passengerRepository.save(ps);
+        return new ResponseEntity<>(ps, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/passenger/{id}")
+    private ResponseEntity<String> deletePassenger(@PathVariable long id) {
+        passengerRepository.deleteById(id);
+        return new ResponseEntity<>("Deleted successfully", HttpStatus.OK);
+    }
+}
--- src/main/java/com/simple/SpringCRUD/model/Passenger.java
+++ src/main/java/com/simple/SpringCRUD/model/Passenger.java
@@ -8,9 +8,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "Passenger")
 public class Passenger {
