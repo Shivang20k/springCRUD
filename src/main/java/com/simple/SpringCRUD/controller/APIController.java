4fb6c7cdcdb9a84390a4b3434d06bcf5cfb091bb
Shivang <st.shivanggupta2@gmail.com>
2023-10-05 22:58:49 +0530

    Adding SPRING_SECURITY & introducing Users-Password to do Basic Authentication but without DB.

--- src/main/java/com/simple/SpringCRUD/controller/APIController.java
+++ src/main/java/com/simple/SpringCRUD/controller/APIController.java
@@ -6,15 +6,9 @@ import com.simple.SpringCRUD.repository.PassengerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -62,5 +56,10 @@ public class APIController {
     private ResponseEntity<String> deletePassenger(@PathVariable long id) {
         passengerRepository.deleteById(id);
         return new ResponseEntity<>("Deleted successfully", HttpStatus.OK);
+    }
+
+    @GetMapping("/current-user")
+    public String getLoggedInUserDetails(Principal principal) {
+        return principal.getName();
     }
 }
